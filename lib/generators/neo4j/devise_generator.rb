91d3dfe1f91d663956401d63ebd5fa604033f49e
Tom Chen <developer@tomchentw.com>
2012-08-22 15:52:40 +0800

    Fix property with type and defaults

--- lib/generators/neo4j/devise_generator.rb
+++ lib/generators/neo4j/devise_generator.rb
@@ -26,39 +26,40 @@ CONTENT
       def migration_data
 <<RUBY
   ## Database authenticatable
-  property :email, :index => :exact
-  property :encrypted_password
+  property :email,                  :type => String, :default => "", :null => false, :index => :exact
+  property :encrypted_password,     :type => String
 
   validates_presence_of :email
+  validates_uniqueness_of :email
   validates_presence_of :encrypted_password
   
   ## Recoverable
-  property :reset_password_token, :index => :exact
-  property :reset_password_sent_at
+  property :reset_password_token,   :type => String, :index => :exact
+  property :reset_password_sent_at, :type => DateTime
 
   ## Rememberable
-  property :remember_created_at
+  property :remember_created_at,    :type => DateTime
 
   ## Trackable
-  property :sign_in_count
-  property :current_sign_in_at
-  property :last_sign_in_at
-  property :current_sign_in_ip
-  property :last_sign_in_ip
+  property :sign_in_count,          :type => Integer, :default => 0
+  property :current_sign_in_at,     :type => DateTime
+  property :last_sign_in_at,        :type => DateTime
+  property :current_sign_in_ip,     :type => String
+  property :last_sign_in_ip,        :type => String
 
   ## Confirmable
-  # property :confirmation_token, :index => :exact
-  # property :confirmed_at
-  # property :confirmation_sent_at
-  # property :unconfirmed_email, :index => :exact # Only if using reconfirmable
+  # property :confirmation_token,   :type => String, :index => :exact
+  # property :confirmed_at,         :type => DateTime
+  # property :confirmation_sent_at, :type => DateTime
+  # property :unconfirmed_email,    :type => String, :index => :exact # Only if using reconfirmable
 
   ## Lockable
-  # property :failed_attempts # Only if lock strategy is :failed_attempts
-  # property :unlock_token, :index => :exact # Only if unlock strategy is :email or :both
-  # property :locked_at
+  # property :failed_attempts,      :type => Integer, :default => 0    # Only if lock strategy is :failed_attempts
+  # property :unlock_token,         :type => String, :index => :exact # Only if unlock strategy is :email or :both
+  # property :locked_at,            :type => DateTime
 
   ## Token authenticatable
-  # property :authentication_token, :index => :exact
+  # property :authentication_token, :type => String, :index => :exact
 RUBY
       end
     end
