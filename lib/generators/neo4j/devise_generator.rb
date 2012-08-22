382a27d24694ef846a5b976d75f9ab62a3e70330
Tom Chen <developer@tomchentw.com>
2012-08-22 15:33:41 +0800

    Add migration properties

--- lib/generators/neo4j/devise_generator.rb
+++ lib/generators/neo4j/devise_generator.rb
@@ -20,7 +20,47 @@ CONTENT
         indent_depth = class_path.size - 1
         content = content.split("\n").map { |line| "  " * indent_depth + line } .join("\n") << "\n"
 
-        inject_into_class(model_path, class_path.last, content) if model_exists?end
+        inject_into_class(model_path, class_path.last, content) if model_exists?
+      end
+
+      def migration_data
+<<RUBY
+  ## Database authenticatable
+  property :email, :index => :exact
+  property :encrypted_password
+
+  validates_presence_of :email
+  validates_presence_of :encrypted_password
+  
+  ## Recoverable
+  property :reset_password_token, :index => :exact
+  property :reset_password_sent_at
+
+  ## Rememberable
+  property :remember_created_at
+
+  ## Trackable
+  property :sign_in_count
+  property :current_sign_in_at
+  property :last_sign_in_at
+  property :current_sign_in_ip
+  property :last_sign_in_ip
+
+  ## Confirmable
+  # property :confirmation_token, :index => :exact
+  # property :confirmed_at
+  # property :confirmation_sent_at
+  # property :unconfirmed_email, :index => :exact # Only if using reconfirmable
+
+  ## Lockable
+  # property :failed_attempts # Only if lock strategy is :failed_attempts
+  # property :unlock_token, :index => :exact # Only if unlock strategy is :email or :both
+  # property :locked_at
+
+  ## Token authenticatable
+  # property :authentication_token, :index => :exact
+RUBY
+      end
     end
   end
 end
